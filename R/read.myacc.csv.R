60de98e98bda1d9ffc1dacca6627e507beb96494
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2021-12-23 09:19:11 +0100

    Update read.myacc.csv.R

--- R/read.myacc.csv.R
+++ R/read.myacc.csv.R
@@ -244,10 +244,13 @@ read.myacc.csv = function(rmc.file=c(), rmc.nrow=c(), rmc.skip=c(), rmc.dec=".",
     accelRes = resample(rawAccel, rawTime, timeRes, rawLast, interpolationType) # this is now the resampled acceleration data
     colnamesP = colnames(P)
     timeRes = as.POSIXlt(timeRes, origin=rmc.origin, tz = rmc.desiredtz)
+    if(all(is.nan(accelRes[1,]))){
+        accelRes = accelRes[-1,]
+        timeRes = timeRes[-1] 
+    } 
     P = as.data.frame(accelRes, stringsAsFactors = TRUE)
     P$timestamp = timeRes
     P = P[,c(ncol(P),1:(ncol(P)-1))]
-    if(all(is.nan(accelRes[1,]))) accelRes = accelRes[-1,]
     colnames(P) = colnamesP
   }
   return(list(data=P,header=header))
